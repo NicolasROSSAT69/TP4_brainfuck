8b8c175dd57c80dceb8b7f3739c40f285ee59b31
nicolas <nicolas.rossat@hotmail.fr>
2022-05-04 09:13:00 +0200

    Les boucles

--- tp4/src/main.rs
+++ tp4/src/main.rs
@@ -5,13 +5,14 @@ enum Instruction {
     Gauche,
     Affiche,
     Lis,
+    Boucle(Vec<Instruction>),
 }
 
 fn interpreteur(memoire: &mut Vec<i32>, instructions: &Vec<Instruction>) {
     let mut compteur = 0;
     let mut position = 0;
     while compteur < instructions.len() {
-        match instructions[compteur] {
+        match &instructions[compteur] {
             Instruction::Plus => {
                 memoire[position] += 1;
             }
@@ -33,6 +34,11 @@ fn interpreteur(memoire: &mut Vec<i32>, instructions: &Vec<Instruction>) {
                 std::io::stdin().read_line(&mut input).unwrap();
                 memoire[position] = input.trim().parse::<i32>().unwrap();
             }
+            Instruction::Boucle(instructions) => {
+                while memoire[position] != 0 {
+                    interpreteur(memoire, &instructions);
+                }
+            }
         }
         compteur += 1;
     }
@@ -40,55 +46,68 @@ fn interpreteur(memoire: &mut Vec<i32>, instructions: &Vec<Instruction>) {
 
 fn main() {
     //Test 1
-    let mut mem = vec![0, 0, 0];
+    // let mut mem = vec![0, 0, 0];
 
-    let instructions = vec![
-        Instruction::Plus,
-        Instruction::Plus,
-        Instruction::Plus,
-        Instruction::Droite,
-        Instruction::Plus,
-        Instruction::Droite,
+    // let instructions = vec![
+    //     Instruction::Plus,
+    //     Instruction::Plus,
+    //     Instruction::Plus,
+    //     Instruction::Droite,
+    //     Instruction::Plus,
+    //     Instruction::Droite,
+    //     Instruction::Moins,
+    // ];
+
+    // interpreteur(&mut mem, &instructions);
+
+    // println!("{:?}", mem);
+
+    // //Test 2
+    // let mut mem2 = vec![72, 101, 108, 108, 111];
+
+    // let instructions2 = vec![
+    //     Instruction::Affiche,
+    //     Instruction::Droite,
+    //     Instruction::Affiche,
+    //     Instruction::Droite,
+    //     Instruction::Affiche,
+    //     Instruction::Droite,
+    //     Instruction::Affiche,
+    //     Instruction::Droite,
+    //     Instruction::Affiche,
+    // ];
+
+    // interpreteur(&mut mem2, &instructions2);
+
+    // //Test 3
+    // let mut mem3 = vec![72, 101, 108, 108, 111];
+
+    // let instructions3 = vec![
+    //     Instruction::Lis,
+    //     Instruction::Plus,
+    //     Instruction::Affiche,
+    //     Instruction::Lis,
+    //     Instruction::Plus,
+    //     Instruction::Affiche,
+    //     Instruction::Lis,
+    //     Instruction::Plus,
+    //     Instruction::Lis,
+    //     Instruction::Plus,
+    //     Instruction::Affiche,
+    // ];
+
+    // interpreteur(&mut mem3, &instructions3);
+
+    //Test 4
+    let mut mem4 = vec![10, 15];
+    let instructions4 = vec![Instruction::Boucle(vec![
         Instruction::Moins,
-    ];
-
-    interpreteur(&mut mem, &instructions);
-
-    println!("{:?}", mem);
-
-    //Test 2
-    let mut mem2 = vec![72, 101, 108, 108, 111];
-
-    let instructions2 = vec![
-        Instruction::Affiche,
         Instruction::Droite,
-        Instruction::Affiche,
-        Instruction::Droite,
-        Instruction::Affiche,
-        Instruction::Droite,
-        Instruction::Affiche,
-        Instruction::Droite,
-        Instruction::Affiche,
-    ];
-
-    interpreteur(&mut mem2, &instructions2);
-
-    //Test 3
-    let mut mem3 = vec![72, 101, 108, 108, 111];
-
-    let instructions3 = vec![
-        Instruction::Lis,
         Instruction::Plus,
-        Instruction::Affiche,
-        Instruction::Lis,
-        Instruction::Plus,
-        Instruction::Affiche,
-        Instruction::Lis,
-        Instruction::Plus,
-        Instruction::Lis,
-        Instruction::Plus,
-        Instruction::Affiche,
-    ];
+        Instruction::Gauche,
+    ])];
 
-    interpreteur(&mut mem3, &instructions3);
+    interpreteur(&mut mem4, &instructions4);
+
+    println!("{:?}", mem4);
 }
