ef006b47b20949af4ed674c6b122d8ac25a3fbf6
nicolas <nicolas.rossat@hotmail.fr>
2022-05-04 08:48:56 +0200

    Entrée / sortie

--- tp4/src/main.rs
+++ tp4/src/main.rs
@@ -24,15 +24,24 @@ fn interpreteur(memoire: &mut Vec<i32>, instructions: &Vec<Instruction>) {
             Instruction::Gauche => {
                 position -= 1;
             }
-            Instruction::Affiche => println!("Vide"),
-            Instruction::Lis => println!("Vide"),
+            Instruction::Affiche => {
+                println!("{}", memoire[position])
+            }
+            Instruction::Lis => {
+                let mut input = String::new();
+                println!("Entrez un nombre : ");
+                std::io::stdin().read_line(&mut input).unwrap();
+                memoire[position] = input.trim().parse::<i32>().unwrap();
+            }
         }
         compteur += 1;
     }
 }
 
 fn main() {
+    //Test 1
     let mut mem = vec![0, 0, 0];
+
     let instructions = vec![
         Instruction::Plus,
         Instruction::Plus,
@@ -42,7 +51,44 @@ fn main() {
         Instruction::Droite,
         Instruction::Moins,
     ];
+
     interpreteur(&mut mem, &instructions);
 
     println!("{:?}", mem);
+
+    //Test 2
+    let mut mem2 = vec![72, 101, 108, 108, 111];
+
+    let instructions2 = vec![
+        Instruction::Affiche,
+        Instruction::Droite,
+        Instruction::Affiche,
+        Instruction::Droite,
+        Instruction::Affiche,
+        Instruction::Droite,
+        Instruction::Affiche,
+        Instruction::Droite,
+        Instruction::Affiche,
+    ];
+
+    interpreteur(&mut mem2, &instructions2);
+
+    //Test 3
+    let mut mem3 = vec![72, 101, 108, 108, 111];
+
+    let instructions3 = vec![
+        Instruction::Lis,
+        Instruction::Plus,
+        Instruction::Affiche,
+        Instruction::Lis,
+        Instruction::Plus,
+        Instruction::Affiche,
+        Instruction::Lis,
+        Instruction::Plus,
+        Instruction::Lis,
+        Instruction::Plus,
+        Instruction::Affiche,
+    ];
+
+    interpreteur(&mut mem3, &instructions3);
 }
