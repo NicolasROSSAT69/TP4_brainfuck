ecb500113b889e02fb964ee17d77d65cf0bc367f
nicolas <nicolas.rossat@hotmail.fr>
2022-05-04 08:25:11 +0200

    Type de base & Interpréteur simple de base

--- tp4/src/main.rs
+++ tp4/src/main.rs
@@ -1,3 +1,48 @@
+enum Instruction {
+    Plus,
+    Moins,
+    Droite,
+    Gauche,
+    Affiche,
+    Lis,
+}
+
+fn interpreteur(memoire: &mut Vec<i32>, instructions: &Vec<Instruction>) {
+    let mut compteur = 0;
+    let mut position = 0;
+    while compteur < instructions.len() {
+        match instructions[compteur] {
+            Instruction::Plus => {
+                memoire[position] += 1;
+            }
+            Instruction::Moins => {
+                memoire[position] -= 1;
+            }
+            Instruction::Droite => {
+                position += 1;
+            }
+            Instruction::Gauche => {
+                position -= 1;
+            }
+            Instruction::Affiche => println!("Vide"),
+            Instruction::Lis => println!("Vide"),
+        }
+        compteur += 1;
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut mem = vec![0, 0, 0];
+    let instructions = vec![
+        Instruction::Plus,
+        Instruction::Plus,
+        Instruction::Plus,
+        Instruction::Droite,
+        Instruction::Plus,
+        Instruction::Droite,
+        Instruction::Moins,
+    ];
+    interpreteur(&mut mem, &instructions);
+
+    println!("{:?}", mem);
 }
